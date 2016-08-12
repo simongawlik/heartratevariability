b7d58032976f580f2f5ce3d4f314cc3a2a736540
Simon Gawlik <sgawlik@gmail.com>
2016-08-11 20:08:04 -0400

    trying to automate login to suunto

--- hrv.py
+++ hrv.py
@@ -1,7 +1,63 @@
 
+from bs4 import BeautifulSoup
 from openpyxl import load_workbook
-import os
+import settings
 import math
+import os
+import requests
+import robobrowser
+
+#login
+#http://www.movescount.com/auth?redirect_uri=%2foverview
+
+br = robobrowser.RoboBrowser(history=True)
+
+url = "http://www.movescount.com/auth?redirect_uri=%2foverview"
+
+br.open(url)
+forms = br.get_forms()
+
+print(forms)
+print()
+print()
+
+form = br.get_form(action="./auth?redirect_uri=%2foverview")
+
+print(form)
+print()
+print()
+
+form['email'].value = settings.email
+form['password'].value = settings.password
+#br.session.headers['Referer'] = url
+
+form["__VIEWSTATE"].value = "/wEPDwUKLTc2MTAxNzQ4MmRkr/lAhZYueBNtINpuoLA7vkegEho="
+form["__VIEWSTATEGENERATOR"].value = "CB4E55BE"
+form.serialize()
+print(form)
+
+br.submit_form(form)
+
+
+
+
+
+
+
+
+'''
+#"http://www.movescount.com/latestmove"
+response = requests.get("http://www.movescount.com/latestmove")
+#print(response.headers)
+#"export?id=117769218&format=xlsx"
+
+html = response.text
+#print(html)
+
+soup = BeautifulSoup(html, "html.parser")
+
+print(soup.find_all("link", limit=3))
+
 
 data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "workbooks")
 file_name = "Move_2016_08_10_16_46_13.xlsx"
@@ -36,3 +92,4 @@ variance = sum_of_squared_errors / (measurement_length - 1)
 std_deviation = math.sqrt(variance)
 
 print(std_deviation)
+'''
